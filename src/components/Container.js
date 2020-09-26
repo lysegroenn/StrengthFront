2c479641929a49ab4bf5056087fb752cc2c9a9ae
Adam Lidén <lysegroenn@gmail.com>
2020-09-26 09:23:52 +0200

    playing with multiple slide ins

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { Transition } from 'react-transition-group';
 import {gsap, TweenMax, Power3 } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -54,11 +53,45 @@ const SlidingContent = styled.div`
                     toggleActions: 'play none none reverse' 
                 }
         });
-              
+
+        mockRefs.current.forEach((el, index) => {
+
+            const isLeft = ((index%4)<2)
+            const initX = isLeft ? -200 : 200;
+            TweenMax.fromTo(el, {
+                autoAlpha: 0,
+                x: initX
+            }, {
+                duration: .5,
+                autoAlpha: 1,
+                ease: 'none',
+                x: 0,
+                scrollTrigger: {
+                    id: `moock-item${index}`,
+                    trigger: el,
+                    start: 'top center+=200',
+                    toggleActions: 'play none none reverse',
+                    markers: false
+                }
+            })
+
+        })
+
+
     }, [])
 
+    const mockContent = [1, 2, 3, 4, 5, 6, 7, 8];
     let SlideItem = useRef(null);
     let SlideItem2 = useRef(null);
+
+    const mockRefs = useRef([]);
+    mockRefs.current = [];
+
+    const addToRefs = (el) => {
+        if(el && !mockRefs.current.includes(el)){
+            mockRefs.current.push(el);
+        }
+    }
 
     return (
         <StyledContainer>
@@ -67,8 +100,11 @@ const SlidingContent = styled.div`
                     <SlidingContent className={`sliding-content-${state}`} />
                 )}
                 </Transition>  */}
-            <SlidingContent style={{'grid-column': '2/3', 'grid-row': '3/4'}} ref={el => {SlideItem2 =el} } />
-            <SlidingContent style={{'grid-column': '2/3', 'grid-row': '4/5'}} ref={el => {SlideItem = el} } />
+            <SlidingContent style={{'gridColumn': '2/3', 'gridRow': '3/4'}} ref={el => {SlideItem2 =el} }></SlidingContent>
+            <SlidingContent style={{'gridColumn': '2/3', 'gridRow': '4/5'}} ref={el => {SlideItem = el} } />
+            <div id='mock-container'>
+                {mockContent.map((e, i) => (<div className='mock-item' ref={addToRefs} key={i}>{e}</div>))}
+            </div>
         </StyledContainer>
     )
 };
